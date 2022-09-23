6d98f6a98c9e29ac019136b58e23ae4353166b91
lylyzyta <lylyzyta@gmail.com>
2022-09-23 13:54:34 -0500

    Create Navbar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+/* import { Home } from "./components/Startbootstrap";
+import { Category } from "./components/Portafolio";
+import { Aboutme } from "./components/About";
+import { Search } from "./components/Contactme";
+import { Footer } from "./components/Footer"; */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <Navbar />
+      </section>
+     {/*  <section>
+        <Home />
+      </section>
+      <section>
+        <Category />
+      </section>
+      <section>
+        <Aboutme />
+      </section>
+      <section>
+        <Search />
+      </section>
+      <section>
+        <Footer />
+      </section> */}
     </div>
   );
 }
