a314f8570343988c00c3d374cca130569cb99dbd
lylyzyta <lylyzyta@gmail.com>
2022-09-26 22:32:58 -0500

    Create Popup component

---
+++ src/components/Popup.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Popup.module.css"
+ 
+const Popup = props => {
+  return (
+    <div className={styles.popupBox}>
+      <div className={styles.box}>
+        <span className={styles.closeIcon} onClick={props.handleClose}>x</span>
+        {props.content}
+      </div>
+    </div>
+  );
+};
+ 
+export default Popup;
--- src/components/PostsBundle.jsx
+++ src/components/PostsBundle.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import getBlogs from "../helpers/api"
 import styles from "./PostsBundle.module.css";
 import dateFormat from 'dateformat';
+import Popup from "./Popup";
 
 function PostsBundle() {
   const [blogs, setBlogs] = useState([]);
@@ -10,6 +11,25 @@ function PostsBundle() {
       "https://6328f6acd2c97d8c525f8f80.mockapi.io/api/v1/blogs?page=1&limit=5"
     ).then((json) => setBlogs(json));
   }, []);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [blogID, setBlogID] = useState([]);
+  const [blogUnique, setBlogUnique] = useState([]);
+  
+  const togglePopup = (props) => {
+    
+     setIsOpen(!isOpen);
+     setBlogID(props)
+          
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+    getBlogs(
+     // eslint-disable-next-line no-template-curly-in-string
+     
+     `https://6328f6acd2c97d8c525f8f80.mockapi.io/api/v1/blogs/${blogID}`
+   ).then((json) => setBlogUnique(json));
+}, []);
+}
 
   return (
     <section className={styles.blogsContainer}>
@@ -21,7 +41,16 @@ function PostsBundle() {
       <section >
         <div className={styles.cardContainer}>
           {blogs.map((blog) => (
-            <section  className={styles.cardStyle} key={blogs.id}>
+             <section  className={styles.cardStyle} key={blogs.id}  onClick={() => togglePopup(blog.id)}>
+                {isOpen && <Popup 
+      content={ 
+       <><b>{blogUnique}</b>
+        <p>{blog.content}</p>
+        <button>Close</button>
+      </>        
+       }
+      handleClose={togglePopup}
+    />}
             <>
               <img
                 className={styles.blogsImg}
